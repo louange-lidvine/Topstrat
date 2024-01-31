a7a788f3182b3fd09bedf789b4aad61725028a9b
A-leslie <anneuhiriwe@gmail.com>
2024-01-31 09:25:12 +0200

    sidebar end

--- src/app/components/Sidebar/page.tsx
+++ src/app/components/Sidebar/page.tsx
@@ -1,14 +1,21 @@
-import React, { Children } from "react";
+"use client"
+import React, { useState} from "react";
 import user from '../../../../public/assets/user.png'
 import Profile from "../profile/page";
 import { FaPlus } from "react-icons/fa";
 import Choose from "../chooseMethod/page";
 
 
+
 function Sidebar() {
+  const [chooseVisible,setChooseVisible]=useState(false)
+
+  const handleClick =()=>{
+    setChooseVisible(!chooseVisible)
+  }
+  
   return (
       <body className="flex items-center">
-
           <div className="flex h-screen text-white">
               <div className="flex-shrink-0 w-64 bg-blue-default m-4 rounded-md">
                 <div className="user-part h-56">
@@ -17,10 +24,15 @@ function Sidebar() {
                 <hr className="w-56 ml-3" />
                 <div className="lower-part">
                  <div className="projects">
-                   <div className="title flex h-56">
+                   <div className="title grid grid-cols-2 space-x-16 h-56">
                     <h1 className="mt-2 ml-10 text-xl font-bold flex-[0.8]">Projects</h1>
-                    <FaPlus className="mt-4"/>
+                    <FaPlus className="mt-4"
+                   onClick={handleClick}
+                    />
+                
+                 {chooseVisible && <Choose/>}  
                    </div>
+                   
                  </div> 
                  <h2 className="mt-2 ml-10 font-bold">Settings</h2>
                  <h2 className="mt-2 ml-10 font-bold">Logout</h2>
--- src/app/components/chooseMethod/page.tsx
+++ src/app/components/chooseMethod/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 function Choose() {
   return (
-    <div className='w-56 h-20 bg-white text-black m-4'>
-        <div className='ml-6'>
+      <div className='w-56 bg-white text-black m-4'>
+        <div className='ml-6 pt-2'>
         <div className='flex my-4'>
         <input type="radio" name="quick method" id="" />
        <h2 className='ml-2'>Quick method</h2>  
@@ -13,13 +14,14 @@ function Choose() {
        <input type="radio" name="step" id="" />
        <h2 className='ml-2'>Step by Step </h2> 
      </div>
-      <button type="submit" className='bg-orange-default px-3 py-1'>
+      <button type="submit" className='bg-orange-default px-3 py-1 mr-5 float-right rounded-md'>
         Next
       </button>
       
         </div>
        
-    </div>
+    </div> 
+   
   )
 }
 
