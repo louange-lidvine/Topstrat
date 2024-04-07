7494e13ece23d5ba92c42d3257c2c98e0061be43
A-Leslie <anneuhiriwe@gmail.com>
2024-04-07 22:34:34 +0200

    auth end and resolve mantine

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,53 +1,8 @@
-// import { getCookie } from "cookies-next";
-// import { NextResponse } from "next/server";
-// import { NextRequest } from "next/server";
-// import { jwtDecode } from "jwt-decode";
-// import {toast }from "react-toastify";
-
-// export default function middleware(
-//   req: NextRequest,
-//   res: NextResponse,
-//   next: () => void,
-// ) {
-//   const token = req.cookies.get("token")?.value;
-
-//   // const decoded = jwtDecode(token ?? "");
-//   // try {
-//   //   const currentDate = new Date().toDateString();
-//   //   const decodedDate = new Date(decoded.iat ?? "").toDateString();
-//   //   console.log(`Current date: ${currentDate}`);
-//   //   console.log(`Decoded date: ${decodedDate}`);
-//   // } catch (error) {
-//   //   console.error("Error parsing date:", error);
-//   // }
-
-//   if (
-//     !token &&
-//     req.nextUrl.pathname !== "/Pages/signIn" 
-//     // && req.nextUrl.pathname !== "/"
-//   ) {
-//     return NextResponse.redirect(new URL("/Pages/signIn", req.url));
-//   }
-
-//   if (token && (req.nextUrl.pathname === "/Pages/signIn")) {
-//     const decoded = jwtDecode(token ?? "") as { role: string };
-//     if (decoded.role === "user") {
-//       return NextResponse.redirect(new URL("/components/landingPage", req.url));
-//     } else if (decoded.role === "admin") {
-//       return NextResponse.redirect(new URL("/components/dashboard", req.url));
-//     } else {
-//       toast.error("Invalid Token!");
-//     }
-//   }
-
-//   return NextResponse.next();
-// }
-
 import { getCookie } from "cookies-next";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import { jwtDecode } from "jwt-decode";
-import {toast} from "react-toastify";
+import {toast} from "react-toastify"; 
 
 export default function middleware(
   req: NextRequest,
@@ -73,7 +28,10 @@ export default function middleware(
   }
   if (
     !token &&
-    req.nextUrl.pathname !== "/Pages/signIn" 
+    req.nextUrl.pathname !== "/Pages/signIn" &&
+    req.nextUrl.pathname !== "/Pages/signup" &&
+    req.nextUrl.pathname !== "/Pages/homePage" &&
+    req.nextUrl.pathname !== "/Pages/services"
   ) {
     return NextResponse.redirect(new URL("/Pages/signIn", req.url));
   }
@@ -92,8 +50,8 @@ export default function middleware(
   return NextResponse.next();
 }
 
-// export const config = {
-//   matcher: [
-//     "/((?!api|/problem/*|/suggestion|/|/locales/*|register/*|verify|forgot|reset|_next/static|public|_next/image|favicon.ico|images|logo.svg|logo.png|rca.jpeg|favicon.svg|favicon.png).*)",
-//   ],
-// };
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
