192bf5338360a6547a41c79c8361dc523403c221
louange-lidvine <lidvinelouange@gmail.com>
2024-04-07 23:09:46 +0200

    updated stepbystep method

--- src/app/components/step/page.tsx
+++ src/app/components/step/page.tsx
@@ -1,104 +1,106 @@
-"use client"
-import React, { useState } from 'react';
-import {FaArrowRight} from 'react-icons/fa'
-import ReactModal from 'react-modal';
-import Step from './card';
-import PromptGet from './getResult';
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Background from "../../../../public/assets/bg.png";
+import Image from "next/image";
+import Prompt from "../prompt/page";
 
-const Component1: React.FC = () => <div>The mission of a business refers to a concise statement that outlines the fundamental purpose, core values, and overarching goals of the organization. It defines what the business aims to achieve, whom it serves, and the principles that guide its actions. The mission statement often reflects the company's identity and provides a sense of direction for its activities and decisions.</div>;
-const Component2: React.FC = () => <div> The mission of a business refers to a concise statement that outlines the fundamental purpose, core values, and overarching goals of the organization. It defines what the business aims to achieve, whom it serves, and the principles that guide its actions. The mission statement often reflects the company's identity and provides a sense of direction for its activities and decisions.</div>;
-const Component3: React.FC = () => <div>The mission of a business refers to a concise statement that outlines the fundamental purpose, core values, and overarching goals of the organization. It defines what the business aims to achieve, whom it serves, and the principles that guide its actions. The mission statement often reflects the company's identity and provides a sense of direction for its activities and decisions.</div>;
-const Component4: React.FC = () => <div> The mission of a business refers to a concise statement that outlines the fundamental purpose, core values, and overarching goals of the organization. It defines what the business aims to achieve, whom it serves, and the principles that guide its actions. The mission statement often reflects the company's identity and provides a sense of direction for its activities and decisions.</div>;
-const Component5: React.FC = () => <div>The mission of a business refers to a concise statement that outlines the fundamental purpose, core values, and overarching goals of the organization. It defines what the business aims to achieve, whom it serves, and the principles that guide its actions. The mission statement often reflects the company's identity and provides a sense of direction for its activities and decisions.</div>;
-const Component6: React.FC = () => <div>The mission of a business refers to a concise statement that outlines the fundamental purpose, core values, and overarching goals of the organization. It defines what the business aims to achieve, whom it serves, and the principles that guide its actions. The mission statement often reflects the company's identity and provides a sense of direction for its activities and decisions.</div>;
+function page() {
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const [isSecModalOpen, setIsSecModalOpen] = useState<boolean>(false);
+    const [prompts, setPrompts] = useState<string[]>([]);
+    const create = () => {
+        useEffect(() => {
+            const fetchData = async () => {
+                try {
+                    const response = await axios.get(
+                        `https://topstrat-backend.onrender.com/projects/create}`
+                    );
+                    setPrompts(response.data);
+                } catch (error) {
+                    console.error("Error fetching prompts:", error);
+                }
+            };
 
+            fetchData();
+        }, []);
+    };
 
-interface ObjectData {
-  name: string;
-  desc:string;
-  component: React.ComponentType<any>;
+    const [title, setTitle] = useState("untitled");
+    const handleInputChange = (event: {
+        target: { value: React.SetStateAction<string> };
+    }) => {
+        setTitle(event.target.value);
+    };
+
+    const handleButtonClick = () => {
+        setIsModalOpen(false);
+        setIsSecModalOpen(true);
+    };
+
+    return (
+        <div className=" flex lg:w-[1150px] lg:mt-0 mt-10 lg:absolute lg:top-[-1px] lg:left-[320px]">
+            <Image
+                src={Background}
+                className="w-full h-full fixed left-0 top-0 -z-10"
+                alt="background-img"
+            />
+            <div className="">
+                {/* opacity of 25 */}
+                <div className="border my-4 rounded-md mx-2 lg:w-[1150px float-right">
+                    <div className="input flex justify-center">
+                        <input
+                            type="text"
+                            className="border m-4 py-2 outline-none lg:px-10 text-center rounded-md "
+                            value={title}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <h1 className="text-3xl my-40 font-bold text-center text-blue-default">
+                            Strategic plan
+                        </h1>
+                    </div>
+                    <div className="prompt grid lg:grid-cols-2 grid-cols-1">
+                        <Prompt
+                            content="Generate a strategic plan for a rabbit rearing
+project and provide clear objectives"
+                        />
+                        <Prompt
+                            content="Generate a strategic plan for a rabbit rearing
+project and provide clear objectives"
+                        />
+                        <Prompt
+                            content="Generate a strategic plan for a rabbit rearing
+project and provide clear objectives"
+                        />
+                        <Prompt
+                            content="Generate a strategic plan for a rabbit rearing
+project and provide clear objectives"
+                        />
+                    </div>
+                    <div className="input flex justify-between ml-6 lg:w-[1100px] border  m-4 py-3 px-6 rounded-md space-x-5 ">
+                        <div>
+                            <input
+                                type="text"
+                                placeholder="Add a short description"
+                                className="outline-none bg-transparent lg:w-[900px]"
+                            />
+                        </div>
+                        <div>
+                            <button
+                                type="submit"
+                                className="text-blue-default font-bold"
+                                onClick={handleButtonClick}
+                            >
+                                Generate
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
-
-const objects: ObjectData[] = [
-  { name: 'Vision',desc:"The vision is an aspirational statement defining an organization's ideal future." ,component: Component1 },
-  { name: 'Mission',desc:"The mission is an organization's fundamental purpose.", component: Component2 },
-  { name: 'Values',desc:"Values are guiding principles shaping organizational culture." ,component: Component3 },
-  { name: 'Objectives',desc:"Objectives are measurable goals aligned with an organization's mission.", component: Component4 },
-  { name: 'Log frames',desc:"The Logframe (Logical Framework) is a systematic project management tool." ,component: Component5 },
-  { name: 'Implementation strategies',desc:"Strategies are detailed plans to achieve organizational goals.", component: Component6 },
-];
-
-const page: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [isSecModalOpen, setIsSecModalOpen] = useState<boolean>(false);
-  const [selectedObject, setSelectedObject] = useState<ObjectData | null>(null);
-
-  const handleObjectClick = (object: ObjectData) => {
-    setSelectedObject(object);
-    setIsModalOpen(true);
-    setIsSecModalOpen(false)
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-    setSelectedObject(null);
-    setIsSecModalOpen(false)
-  };
-  const handleButtonClick = () => {
-    setIsModalOpen(false);
-    setIsSecModalOpen(true)
-  };
-
-  return (
-    <div className='border lg:mt-4 mt-10 my-4 rounded-md mx-2 lg:w-[1150px] lg:absolute lg:top-[-1px] lg:left-[320px]'>
-         <div className="input flex justify-center">
-         <input type="text" placeholder='untitled' className='border m-4 py-2 outline-none lg:px-10 text-center rounded-md ' />
-        </div>
-        <div className="lg:w-[1100px] grid lg:grid-cols-2 grid-cols-1 gap-4 py-20 lg:px-20 lg:mt-6 lg:ml-10 bg-white">
-          {objects.map((object, index) => (
-          <div key={index} onClick={() => handleObjectClick(object)} className='px-6 my-6 x-8 border w-100 text-lg rounded-md pt-2 pb-6'>
-            <input type="radio" name="check" id="check" />
-            <h2 className='ml-4 font-bold text-blue-default text-center'>{object.name}</h2> 
-            <h2 className='font-md'>{object.desc}</h2>
-            <FaArrowRight className='float-right text-orange-default'/> 
-          </div>
-        ))}
-      </div>
-      <div className="input flex justify-between lg:ml-24 lg:w-[960px] border  m-4 py-3 px-6 rounded-md space-x-5 ">
-        
-         <div> 
-          <input type="text" placeholder='Add a short description' className='outline-none bg-transparent lg:w-[600px]' /></div>
-         <div>     
-          <button type='submit' className='text-blue-default font-bold'>Generate</button></div>
-    
-        </div>
-        
-      <ReactModal isOpen={isModalOpen}
-       onRequestClose={handleCloseModal}
-       className="w-[600px]  p-10 mt-20 bg-white shadow-lg lg:ml-[500px]"
-      >
-        {selectedObject && <Step title={selectedObject.name}  desc={selectedObject.component}/>}
-        <div className="input flex lg:space-x-44 lg:w-[500px] border  my-4 py-3 px-6 rounded-md ">
-         <input type="text" placeholder='Add a short description' className='outline-none bg-transparent w-[300px]' />
-         <button type='submit' className='text-blue-default font-bold' onClick={handleButtonClick}>Generate</button>
-        </div>
-        <button onClick={handleCloseModal}>Close Modal</button>
-
-      </ReactModal>
-
-    <ReactModal isOpen={isSecModalOpen}
-    onRequestClose={handleCloseModal}
-    className="w-[600px]  px-10 py-20 mt-20 bg-white shadow-lg lg:ml-[500px]">
-    {selectedObject && <PromptGet title={selectedObject.name} />}
-    <div className="buttons flex space-x-5 float-end">
-      <button type="submit" className='bg-[#0F872F] py-2 px-4 rounded-md'>Save</button>
-      <button type="submit" className='bg-[#C3BC0D] py-2 px-4 rounded-md'>Enhance</button>
-      <button type="submit" className='bg-[#ED0C0C] py-2 px-4 rounded-md'>Re-generate</button>
-      </div>
-    </ReactModal>
-    </div>
-  );
-};
-
 
 export default page;
