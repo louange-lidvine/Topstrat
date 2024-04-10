84c1327e1ecd69b5eb989c6745e3ec9d050d3d3d
louange-lidvine <lidvinelouange@gmail.com>
2024-04-10 16:17:39 +0200

    finished subscription

--- src/app/Pages/ChoosePayment/package.tsx
+++ src/app/Pages/ChoosePayment/package.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-interface PackageProps{
-    title:string
+import React from "react";
+
+interface PackageProps {
+    title: string;
+    onClick: () => void; // Specify the type of onClick as a function that takes no arguments and returns void
 }
 
-const Package:React.FC<PackageProps> = ({title}) => {
-  return (
-    <div className='border border-blue-default rounded-md  my-6 lg:w-[300px] lg:h-[200px] py-2 px-4'>
-        <h2 className='text-lg font-bold'>{title}</h2>
-    </div>
-  )
-}
+const Package: React.FC<PackageProps> = ({ title, onClick }) => {
+    return (
+        <div
+            className="border border-blue-default rounded-md my-6 lg:w-[300px] lg:h-[200px] py-2 px-4"
+            onClick={onClick}
+        >
+            <h2 className="text-lg font-bold">{title}</h2>
+        </div>
+    );
+};
 
-export default Package
+export default Package;
