df0850a89af629f04367d545d35fa5d4ed9bcb0f
u-leslie <heavenmadeleslie@gmail.com>
2024-04-23 06:35:19 +0200

    feat:add loader for getprompts

--- src/app/shared/loader/page.tsx
+++ src/app/shared/loader/page.tsx
@@ -1,17 +1,42 @@
+// import React from 'react';
+// import { ColorRing } from 'react-loader-spinner';
+
+// const Loader: React.FC = () => {
+//   return (
+//     <div>
+//       <ColorRing
+//          visible={true}
+//          height="60"
+//          width="60"
+//          ariaLabel="color-ring-loading"
+//          wrapperStyle={{}}
+//          wrapperClass="color-ring-wrapper"
+//          colors={['#0B6C79', '#0B6C79', '#0B6C79', '#0B6C79', '#0B6C79']}
+//       />
+//     </div>
+//   );
+// };
+
+// export default Loader;
+
+
+
+// Loader component (Loader.tsx)
+
 import React from 'react';
 import { ColorRing } from 'react-loader-spinner';
 
 const Loader: React.FC = () => {
   return (
-    <div>
+    <div className="flex justify-center items-center bg-opacity-50">
       <ColorRing
-         visible={true}
-         height="60"
-         width="60"
-         ariaLabel="color-ring-loading"
-         wrapperStyle={{}}
-         wrapperClass="color-ring-wrapper"
-         colors={['#0B6C79', '#0B6C79', '#0B6C79', '#0B6C79', '#0B6C79']}
+        visible={true}
+        height={60}
+        width={60}
+        ariaLabel="color-ring-loading"
+        wrapperStyle={{}}
+        wrapperClass="color-ring-wrapper"
+        colors={['#0B6C79', '#0B6C79', '#0B6C79', '#0B6C79', '#0B6C79']}
       />
     </div>
   );
