5ecce6e7495bf8960ad1a246f5589acbc5449885
ivainqueur <ishimvainqueur@gmail.com>
2024-06-01 06:01:06 +0200

    reactpdf build issue

--- src/app/components/Export/PdfButton.tsx
+++ src/app/components/Export/PdfButton.tsx
@@ -1,46 +1,27 @@
-// "use client"
-// import React from "react";
-// import Final from "../Final/[id]/page";
-// import { PDFDownloadLink } from "@react-pdf/renderer";
-
-// const PdfButton = () => {
-//     return (
-//         <PDFDownloadLink document={<Final />} fileName="document.pdf">
-//             {({ blob, url, loading, error }) =>
-//                 loading ? (
-//                     <div>Loading...</div>
-//                 ) : (
-//                     <button className="bg-[#FBBC05] text-white font-bold rounded-md py-3 px-6 flex items-center justify-center my-5 mx-auto">
-//                         Export as PDF
-//                     </button>
-//                 )
-//             }
-//         </PDFDownloadLink>
-//     );
-// };
-
-// export default PdfButton;
-
-
+"use client";
 import React from "react";
 import Final from "../Final/[id]/page";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 
-
-const PdfButton = () => { // Receive id as props
-    return (
-        <PDFDownloadLink document={<Final  />} fileName="document.pdf"> 
-            {({ blob, url, loading, error }) =>
-                loading ? (
-                    <div>Loading...</div>
-                ) : (
-                    <button className="bg-[#FBBC05] text-white font-bold rounded-md py-3 px-6 flex items-center justify-center my-5 mx-auto">
-                        Export as PDF
-                    </button>
-                )
-            }
+const PdfButton = () => {
+  // Receive id as props
+  return (
+    <>
+      {typeof window !== "undefined" && (
+        <PDFDownloadLink document={<Final />} fileName="document.pdf">
+          {({ blob, url, loading, error }) =>
+            loading ? (
+              <div>Loading...</div>
+            ) : (
+              <button className="bg-[#FBBC05] text-white font-bold rounded-md py-3 px-6 flex items-center justify-center my-5 mx-auto">
+                Export as PDF
+              </button>
+            )
+          }
         </PDFDownloadLink>
-    );
+      )}
+    </>
+  );
 };
 
 export default PdfButton;
