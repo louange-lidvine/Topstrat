92beecee45a6765928982193db6bdbe4f764291f
u-leslie <heavenmadeleslie@gmail.com>
2024-09-04 15:56:02 -0500

    fix:title on pestle

--- src/app/components/Preview2/[id]/page.tsx
+++ src/app/components/Preview2/[id]/page.tsx
@@ -198,7 +198,7 @@ function Preview() {
                                             Influence on organization
                                         </th>
                                         <th className="border border-1 p-2 text-blue-default font-bold text-center">
-                                            Impact on organization
+                                            Impact of organization's activities'
                                         </th>
                                     </tr>
                                 </thead>
