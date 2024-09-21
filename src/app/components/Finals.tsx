4c2d70d49fcc7060742f60cb9a4f2da1cc2b0927
louange-lidvine <lidvinelouange@gmail.com>
2024-09-21 18:28:00 +0200

    made some changes

--- src/app/components/Finals.tsx
+++ src/app/components/Finals.tsx
@@ -8,7 +8,7 @@ import SwotSkeleton from "../components/skeletons/SwotSkeleton";
 import LogFrameSkeleton from "../components/skeletons/LogFrameSkeleton";
 import PestleSkeleton from "../components/skeletons/PestleSkeleton";
 import Skeleton from "react-loading-skeleton";
-import ExportPage from "../components//Export/page";
+
 import { baseURL } from "@/app/constants";
 import ReactModal from "react-modal";
 import { useRouter } from "next/navigation";
@@ -32,8 +32,6 @@ function Finals({ id }: FinalsProps) {
     const [userData, setUserData] = useState<any>(null);
     const [gravatarUrl, setGravatarUrl] = useState<string>(""); // Optional: Gravatar URL
     const [hasWatermark, setHasWatermark] = useState(false); // State for watermark
-
-
 
     useEffect(() => {
         const fetchData = async () => {
@@ -91,73 +89,68 @@ function Finals({ id }: FinalsProps) {
         fetchData();
     }, [id]);
 
-    
-      useEffect(() => {
-          // Fetch project data
-          const getProject = async (id: string) => {
-              try {
-                  const token = getCookie("token");
-                  const response = await axios.get(
-                      `${baseURL}/projects/${id}`,
-                      {
-                          headers: {
-                              "Content-Type": "application/json",
-                              Authorization: `Bearer ${token}`,
-                          },
-                      }
-                  );
-                  console.log("Project data:", response.data);
-                  setProjectData(response.data);
-              } catch (error) {
-                  console.error("Error fetching project data:", error);
-                  setError("Failed to fetch project data.");
-              }
-          };
+    useEffect(() => {
+        // Fetch project data
+        const getProject = async (id: string) => {
+            try {
+                const token = getCookie("token");
+                const response = await axios.get(`${baseURL}/projects/${id}`, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                console.log("Project data:", response.data);
+                setProjectData(response.data);
+            } catch (error) {
+                console.error("Error fetching project data:", error);
+                setError("Failed to fetch project data.");
+            }
+        };
 
-          // Fetch user data using userId from localStorage
-          const getUserData = async () => {
-              try {
-                  const userId = localStorage.getItem("userId"); // Fetch userId from localStorage
-                  const token = getCookie("token");
+        // Fetch user data using userId from localStorage
+        const getUserData = async () => {
+            try {
+                const userId = localStorage.getItem("userId"); // Fetch userId from localStorage
+                const token = getCookie("token");
 
-                  if (userId) {
-                      const response = await axios.get(
-                          `${baseURL}/users/${userId}`,
-                          {
-                              headers: {
-                                  "Content-Type": "application/json",
-                                  Authorization: `Bearer ${token}`,
-                              },
-                          }
-                      );
-                      console.log("User data:", response.data);
-                      setUserData(response.data);
+                if (userId) {
+                    const response = await axios.get(
+                        `${baseURL}/users/${userId}`,
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ${token}`,
+                            },
+                        }
+                    );
+                    console.log("User data:", response.data);
+                    setUserData(response.data);
 
-                      // Optional: If Gravatar URL is part of user data
-                      if (response.data.gravatar) {
-                          setGravatarUrl(response.data.gravatar);
-                      }
+                    // Optional: If Gravatar URL is part of user data
+                    if (response.data.gravatar) {
+                        setGravatarUrl(response.data.gravatar);
+                    }
 
-                      // Check subscription type for watermark
-                      if (response.data.subscription === "FreeTrial") {
-                          setHasWatermark(true);
-                      } else {
-                          setHasWatermark(false);
-                      }
-                  } else {
-                      console.error("User ID not found in localStorage.");
-                  }
-              } catch (error) {
-                  console.error("Error fetching user data:", error);
-                  setError("Failed to fetch user data.");
-              }
-          };
+                    // Check subscription type for watermark
+                    if (response.data.subscription === "FreeTrial") {
+                        setHasWatermark(true);
+                    } else {
+                        setHasWatermark(false);
+                    }
+                } else {
+                    console.error("User ID not found in localStorage.");
+                }
+            } catch (error) {
+                console.error("Error fetching user data:", error);
+                setError("Failed to fetch user data.");
+            }
+        };
 
-          getProject(id as string);
-          getUserData();
-          setIsLoading(false);
-      }, [id]);
-
+        getProject(id as string);
+        getUserData();
+        setIsLoading(false);
+    }, [id]);
 
     const regenerateData = async () => {
         try {
@@ -217,8 +210,6 @@ function Finals({ id }: FinalsProps) {
         setIsModalOpen(true);
     };
 
-
-
     const renderList = (data: string) => {
         return (
             <ul style={{ paddingLeft: "20px", listStyleType: "disc" }}>
@@ -240,7 +231,6 @@ function Finals({ id }: FinalsProps) {
             </ul>
         );
     };
-    
 
     const renderTextWithBold = (text: string) => {
         const parts = text.split(/\*\*(.*?)\*\*/g);
@@ -258,8 +248,9 @@ function Finals({ id }: FinalsProps) {
             <Page size="A4" style={{ margin: "auto" }}>
                 <div
                     className={`border  border-blue-default my-4 rounded-md mx-2 p-4 font-medium ${
-                hasWatermark ? "watermarked" : "" }`}
-                    id={`pdf-content_${id}`}  
+                        hasWatermark ? "watermarked" : ""
+                    }`}
+                    id={`pdf-content_${id}`}
                 >
                     <div
                         className="justify-end flex gap-2 cursor-pointer"
